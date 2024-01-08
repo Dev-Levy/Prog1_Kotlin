c3f835df133d383caf4de5d225e9cfd584894f2c
Oláh Levente <levente@stud.uni-obuda.hu>
2024-01-08 15:43:28 +0100

    complex algos done

---
+++ src/ComplexProgAlgos.kt
@@ -0,0 +1,131 @@
+class ComplexProgAlgos {
+
+    fun negativecopy(array: Array<Int>): Array<Int> {
+        val out = Array(array.size) { 0 }
+        for (i in array.indices) {
+            out[i] = array[i] * (-1)
+        }
+        return out
+    }
+    fun arrayEvenSelection(array: Array<Int>): Pair<Array<Int>, Int> {
+        var out = Array(array.size) { 0 }
+        var db = 0
+
+        for (i in array.indices) {
+            if (array[i] % 2 == 0) {
+                out[db] = array[i]
+                db++
+            }
+        }
+
+        return Pair(out, db)
+    }
+    fun arrayEvenSelectionInTheArray(array: Array<Int>): Pair<Array<Int>, Int> {
+        var db = 0
+
+        for (i in array.indices) {
+            if (array[i] % 2 == 0) {
+                var help = array[db]
+                array[db] = array[i]
+                array[i] = help
+                db++
+            }
+        }
+
+        return Pair(array, db)
+    }
+    fun arrayEvenSeparationInTheArray(array: Array<Int>): Int {
+        var bal = 0
+        var jobb = array.size - 1
+        val seged = array[0]
+        while (bal < jobb) {
+            while (bal < jobb && array[jobb] % 2 != 0) jobb--
+            if (bal < jobb) {
+                array[bal] = array[jobb]
+                bal++
+            }
+            while (bal < jobb && array[bal] % 2 == 0) bal++
+            if (bal < jobb) {
+                array[jobb] = array[bal]
+                jobb--
+            }
+        }
+        array[bal] = seged
+        if (seged % 2 == 0) return bal
+        else return bal - 1
+
+    }
+    fun intersection(array1: Array<Int>, array2: Array<Int>): Pair<Array<Int>, Int> {
+        val intersection = Array(array1.size) { 0 }
+        var db = 0
+        for (i in array1.indices) {
+            var j = 0
+            while (j < array2.size && array1[i] != array2[j]) j++
+            if (j < array2.size) {
+                intersection[db] = array1[i]
+                db++
+            }
+        }
+
+        return Pair(intersection, db)
+    }
+    fun union(array1: Array<Int>, array2: Array<Int>): Pair<Array<Int>, Int> {
+        val union = Array(array1.size + array2.size) { 0 }
+        for (i in array1.indices) {
+            union[i] = array1[i]
+        }
+        var db = array1.size
+        for (j in array2.indices) {
+            var i = 0
+            while (i < array1.size && array1[i] != array2[j]) i++
+            if (i >= array1.size) {
+                union[db] = array2[j]
+                db++
+            }
+        }
+        return Pair(union, db)
+    }
+    fun setMaker(array: Array<Int>): Int {
+        var db = 1
+        for (i in 1..array.size - 1) {
+            var j = 0
+            while (j <= db && array[i] != array[j])
+                j++
+            if (j > db) {
+                db++
+                array[db] = array[i]
+            }
+        }
+        return db + 1
+    }
+    fun runTogether(array1: Array<Int>, array2: Array<Int>): Pair<Array<Int>, Int> {
+        val runtogether = Array(array1.size + array2.size) { 0 }
+        var i = 0
+        var j = 0
+        var db = 0
+        while (i < array1.size && j < array2.size) {
+            if (array1[i] < array2[j]) {
+                runtogether[db] = array1[i++]
+            }
+            else if (array1[i] > array2[j]) {
+                runtogether[db] = array2[j++]
+            }
+            else {
+                runtogether[db] = array1[i]
+                i++
+                j++
+            }
+            db++
+        }
+        while (i < array1.size) {
+            runtogether[db] = array1[i++]
+            db++
+        }
+        while (j < array2.size) {
+            runtogether[db] = array2[j++]
+            db++
+        }
+
+        return Pair(runtogether, db)
+    }
+}
--- src/Main.kt
+++ src/Main.kt
@@ -3,70 +3,125 @@ fun main() {
     val numArray = arrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9)
     val oddArray = arrayOf(1, 3, 5, 7, 9)
     val almostOddArray = arrayOf(1, 3, 5, 7, 8, 9)
-    val randomArray = arrayOf(5,2,8,1,9,0,1,2,5)
+    val randomArray = arrayOf(5, 2, 2, 8, 1, 9, 0, 1, 4, 5)
 
-    val simple = SimpleProgAlgos()
+    val sortedArray3 = arrayOf(1,4,7,10,13,16)
+    val sortedArray2 = arrayOf(0,2,4,6,8,10)
 
-    arrayPrint(numArray)
-    val sum = simple.summation(numArray)
-    println("The sum of numbers in array: $sum")
+    //region simple algos
+//
+//    val simple = SimpleProgAlgos()
+//
+//    arrayPrint(numArray)
+//    val sum = simple.summation(numArray)
+//    println("The sum of numbers in array: $sum")
+//
+//    println()
+//
+//    val odd = simple.decision(oddArray)
+//    arrayPrint(oddArray)
+//    println("The array contains an even number?\t$odd")
+//
+//    println()
+//
+//    val notOdd = simple.decision(almostOddArray)
+//    arrayPrint(almostOddArray)
+//    println("The array contains an even number?\t$notOdd")
+//
+//    println()
+//
+//    arrayPrint(oddArray)
+//    val idx_odd = simple.selection(oddArray)
+//    if (idx_odd == 0)
+//        println("No even numbers in array!")
+//    else
+//        println("$idx_odd. number is even!")
+//
+//    println()
+//
+//    arrayPrint(almostOddArray)
+//    val idx_even = simple.selection(almostOddArray)
+//    if (idx_even == 0)
+//        println("No even numbers in array!")
+//    else
+//        println("$idx_even. number is even!")
+//
+//    println()
+//
+//    val N = 23
+//    val prime = simple.primeTest(N)
+//    println("$N is prime? $prime")
+//
+//    println()
+//
+//    arrayPrint(almostOddArray)
+//    val search = simple.linearSearch(almostOddArray)
+//    if (search > 0)
+//        println("Even number at ${search+1}. place in array")
+//    else
+//        println("No even numbers in the array")
+//
+//    println()
+//
+//    arrayPrint(numArray)
+//    var count = simple.countEvens(numArray)
+//    println("$count even numbers in this array")
+//
+//    println()
+//
+//    arrayPrint(randomArray)
+//    var max = simple.max(randomArray)
+//    println("Highest number at ${max+1}. place in array")
+//
+    //endregion
 
-    println()
+    //region complex algos
 
-    val odd = simple.decision(oddArray)
-    arrayPrint(oddArray)
-    println("The array contains an even number?\t$odd")
+    val complex = ComplexProgAlgos()
 
-    println()
-
-    val notOdd = simple.decision(almostOddArray)
-    arrayPrint(almostOddArray)
-    println("The array contains an even number?\t$notOdd")
-
-    println()
-
-    arrayPrint(oddArray)
-    val idx_odd = simple.selection(oddArray)
-    if (idx_odd == 0)
-        println("No even numbers in array!")
-    else
-        println("$idx_odd. number is even!")
-
-    println()
-
-    arrayPrint(almostOddArray)
-    val idx_even = simple.selection(almostOddArray)
-    if (idx_even == 0)
-        println("No even numbers in array!")
-    else
-        println("$idx_even. number is even!")
-
-    println()
-
-    val N = 23
-    val prime = simple.primeTest(N)
-    println("$N is prime? $prime")
-
-    println()
-
-    arrayPrint(almostOddArray)
-    val search = simple.linearSearch(almostOddArray)
-    if (search > 0)
-        println("Even number at ${search+1}. place in array")
-    else
-        println("No even numbers in the array")
-
-    println()
-
-    arrayPrint(numArray)
-    var count = simple.countEvens(numArray)
-    println("$count even numbers in this array")
-
+    arrayPrint(randomArray)
+    arrayPrint(complex.negativecopy(randomArray))
     println()
 
     arrayPrint(randomArray)
-    var max = simple.max(randomArray)
-    println("Highest number at ${max+1}. place in array")
+    val pair = complex.arrayEvenSelection(randomArray)
+    println("${pair.second} even numbers in array")
+    arrayPrint(pair.first)
+    println()
+
+//    arrayPrint(randomArray)
+//    val db = complex.arrayEvenSeparationInTheArray(randomArray)
+//    println("${db+1} even number in array")
+//    arrayPrint(randomArray)
+//    println()
+
+
+    arrayPrint(almostOddArray)
+    arrayPrint(randomArray)
+    val intersection = complex.intersection(almostOddArray, randomArray)
+    println("${intersection.second} common numbers in these arrays")
+    arrayPrint(intersection.first)
+    println()
+
+    arrayPrint(almostOddArray)
+    arrayPrint(randomArray)
+    val union = complex.union(almostOddArray, randomArray)
+    println("${union.second} numbers in the union")
+    arrayPrint(union.first)
+    println()
+
+    arrayPrint(randomArray)
+    println("${complex.setMaker(randomArray)} different numbers in array")
+    arrayPrint(randomArray)
+    println()
+
+    arrayPrint(sortedArray2)
+    arrayPrint(sortedArray3)
+    println("${complex.runTogether(sortedArray2,sortedArray3).second} numbers in this array")
+    arrayPrint(complex.runTogether(sortedArray2,sortedArray3).first)
+
+
+    //endregion
 }
 
 fun <T> arrayPrint(array: Array<T>) {
